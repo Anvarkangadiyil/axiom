ee4dfac0f266b9cca18a6dcefdc19a9b7c1769b8
Anvarkangadiyil <125873949+Anvarkangadiyil@users.noreply.github.com>
2026-01-18 15:52:47 +0530

    database setup and clerk connect with convex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,33 @@
-import { Button } from "@/components/ui/button"
+"use client";
+import { Button } from "@/components/ui/button";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
-const x =()=>{
-  return(
+const x = () => {
+  const projects = useQuery(api.projects.get);
+  const createProject = useMutation(api.projects.create);
+         
+  return (
     <div>
-      <Button variant="purple">
-        Click me here
+      <Button
+        onClick={() => {
+          createProject({
+            name: "New Project",
+          });
+        }}
+      >
+        Add
       </Button>
+      {projects?.map((p) => {
+        return (
+          <div key={p._id}>
+            <p>{p.name}</p>
+            <p>{p.ownerId}</p>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
 export default x;
