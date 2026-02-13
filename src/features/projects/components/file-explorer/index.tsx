1733a42b7bfad4ef500d9246c023981d5d7e0a62
Anvarkangadiyil <125873949+Anvarkangadiyil@users.noreply.github.com>
2026-02-14 00:20:26 +0530

    feat: add file explorer

---
+++ src/features/projects/components/file-explorer/index.tsx
@@ -0,0 +1,126 @@
+import { useState } from "react";
+
+import { cn } from "@/lib/utils";
+import {
+  ChevronRightIcon,
+  CopyMinusIcon,
+  FilePlusCorner,
+  FolderPlusIcon,
+} from "lucide-react";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Button } from "@/components/ui/button";
+
+import { Id } from "../../../../../convex/_generated/dataModel";
+import { useProject } from "../../hooks/use-projects";
+import {
+  useCreateFile,
+  useCreateFolder,
+  useFolderContents,
+} from "../../hooks/use-file";
+import { CreateInput } from "./create-input";
+import LoadingRow from "./loading-row";
+import Tree from "./tree";
+
+export const FileExplorer = ({ projectId }: { projectId: Id<"projects"> }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [collapseKey, setCollapseKey] = useState(0);
+  const [creating, setCreating] = useState<"file" | "folder" | null>(null);
+
+  const project = useProject(projectId);
+  const rootFiles = useFolderContents({ projectId, enabled: isOpen });
+
+  const createFile = useCreateFile();
+  const createFolder = useCreateFolder();
+
+  const handleCreate = (name: string) => {
+    setCreating(null);
+    if (creating === "file") {
+      createFile({ projectId, name, content: "", parentId: undefined });
+    } else {
+      createFolder({ projectId, name, parentId: undefined });
+    }
+  };
+
+
+  return (
+    <div className="h-full bg-sidebar">
+      <ScrollArea>
+        <div
+          role="button"
+          onClick={() => {
+            setIsOpen((prev) => !prev);
+          }}
+          className="group/project cursor-pointer w-full text-left flex  items-center gap-0.5 h-5.5 font-bold bg-accent  "
+        >
+          <ChevronRightIcon
+            className={cn(
+              "size-4 shrink-0 text-muted-foreground ",
+              isOpen && "rotate-90",
+            )}
+          />
+          <p className="text-xs uppercase line-clamp-1">
+            {project?.name ?? "Loading..."}
+          </p>
+          <div className="opacity-p group-hover/project:opacity-100 transition-none duration-0 flex items-center gap-0.5 ml-auto opacity-0">
+            <Button
+              size="icon-xs"
+              onClick={(e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                setIsOpen(true);
+                setCreating("file");
+              }}
+              variant="highlight"
+            >
+              <FilePlusCorner className="size-3.5" />
+            </Button>
+            <Button
+              size="icon-xs"
+              onClick={(e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                setCreating("folder");
+              }}
+              variant="highlight"
+            >
+              <FolderPlusIcon className="size-3.5" />
+            </Button>
+
+            <Button
+              size="icon-xs"
+              onClick={(e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                setCollapseKey((prev) => prev + 1);
+              }}
+              variant="highlight"
+            >
+              <CopyMinusIcon className="size-3.5" />
+            </Button>
+          </div>
+        </div>
+        {isOpen && (
+          <>
+            {rootFiles===undefined && <LoadingRow level={0}/>}
+            {creating && (
+              <CreateInput
+                type={creating}
+                level={0}
+                onSubmit={handleCreate}
+                onCancel={() => setCreating(null)}
+              />
+            )}
+            {rootFiles?.map((file) => (
+              <Tree
+                key={`${file._id}-${collapseKey}`}
+                item={file}
+                level={0}
+                projectId={projectId}
+              />
+            ))}
+          </>
+        )}
+      </ScrollArea>
+    </div>
+  );
+};
